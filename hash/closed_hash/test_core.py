7f33f442d848c2097223d8e90fc57dda5affe2d0
박재민 <jmp7786@gmail.com>
2019-09-09 03:18:26 +0900

    closed hash table 1회 습완료

--- hash/closed_hash/test_core.py
+++ hash/closed_hash/test_core.py
@@ -1,4 +1,4 @@
-from hash.closed_hash.core import Chaining
+from hash.closed_hash.core2 import Chaining
 
 def test_core():
     t = Chaining(13)
@@ -14,7 +14,6 @@ def test_core():
     
     assert t.get(50) == 'orange'
     assert t.get(63) == 'watermelon'
-    assert t.get(37) == 'apple'
     assert t.get(37) == 'apple'
     
     assert t.get_attributes() == [[], [], [], [[55, 'cherry']], [], [[18, 'banana']], [], [], [], [[35, 'lime'], [22, 'mango']], [], [[63, 'watermelon'], [50, 'orange'], [37, 'apple']], [[25, 'grape']]]
