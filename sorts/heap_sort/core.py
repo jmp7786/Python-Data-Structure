c31c5045ff2b3936abcd4a859829a20e25703e51
박재민 <jmp7786@gmail.com>
2019-09-11 11:12:04 +0900

    heap sort 습 1차 완료

--- sorts/heap_sort/core.py
+++ sorts/heap_sort/core.py
@@ -1,25 +1,23 @@
-def downheap(a, i, size):
-    while 2*i <= size:
-        k = 2*i
-        if k < size and a[k] < a[k+1]:
+def downheap(a, i, hsize):
+    while i * 2 <= hsize:
+        k = i * 2
+        if k < hsize and a[k] < a[k+1]:
             k += 1
-        if a[i] >= a[k]:
+        if a[i] > a[k]:
             break
-        
+            
         a[i], a[k] = a[k], a[i]
-        i = k
         
+        i = k
+
 def create_heap(a):
     hsize = len(a) - 1
-    for i in reversed(range(1, hsize//+1)):
+    for i in reversed(range(1, hsize//2 +1)):
         downheap(a, i, hsize)
-        
+    
 def heap_sort(a):
-    N = len(a) - 1
-    for i in range(N):
-        a[1], a[N] = a[N], a[1]
-        downheap(a, 1, N-1)
-        N -= 1
-        
-        
-
+    hsize = len(a) - 1
+    for i in range(hsize):
+        a[1], a[hsize] = a[hsize], a[1]
+        downheap(a, 1, hsize-1)
+        hsize -= 1
