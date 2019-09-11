2973a74f2f2a83814341ee9d884d4fb3e73420d2
박재민 <jmp7786@gmail.com>
2019-09-11 10:40:19 +0900

    merge sort 습중

--- sorts/merge_sort/core2.py
+++ sorts/merge_sort/core2.py
@@ -8,7 +8,7 @@ def merge(a, b, low, mid, high):
         elif j > high:
             b[k] = a[i]
             i += 1
-        elif a[i] > a[j]:
+        elif a[j] < a[i]:
             b[k] = a[j]
             j += 1
         else:
@@ -17,13 +17,13 @@ def merge(a, b, low, mid, high):
     
     for k in range(low, high+1):
         a[k] = b[k]
-
+        
 def merge_sort(a, b, low, high):
-    if low >= high:
+    if high <= low:
         return
-    
     mid = low + (high - low) // 2
     merge_sort(a, b, low, mid)
     merge_sort(a, b, mid+1, high)
     merge(a, b, low, mid, high)
+    
     
